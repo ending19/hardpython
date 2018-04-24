8d4377cd38fa254546ffac16fbe8edaf08204081
ending19 <ending19@163.com>
2018-04-24 22:07:43 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,26 @@
+################################################
+# File Name: ex1.py
+# Author: ending19
+# mail: ending19@163.com
+# Created Time: Tue 24 Apr 2018 01:14:20 PM CST
+#======================================
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('Hello world!')
+print('Hello Again!')
+print('I like tpying this.')
+print('This is fun.')
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('')
+
+#同一行显示
+print('Hello world!',end='')
+print('Hello Again!',end='')
+print('I like tpying this.',end='')
+print('This is fun.',end='')
+print('Yay! Printing.',end='')
+print("I'd much rather you 'not'.",end='')
+print('I "said" do not touch this.')
